eecfd313bf9ca184a184f39f7a001475e31e7291
Tirth Sejpal <tsejpal@synoptek.com>
2025-11-07 12:22:16 +0530

    Add .gitignore and clean up ignored files

---
+++ Backend/src/Services/CourseService/CourseService.DAL/Data Context/CourseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseService.DAL.Data_Context
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext(DbContextOptions<CourseContext> options) : base(options)
+        {
+        }
+    }
+}
