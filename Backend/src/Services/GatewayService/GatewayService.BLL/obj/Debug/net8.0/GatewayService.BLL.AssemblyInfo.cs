0af7859fa45c5187c198e322e03017b8474bc09d
Sejpal Tirth <tirths331@gmail.com>
2025-11-17 15:04:13 +0530

    Commit staged

--- Backend/src/Services/GatewayService/GatewayService.BLL/obj/Debug/net8.0/GatewayService.BLL.AssemblyInfo.cs
+++ Backend/src/Services/GatewayService/GatewayService.BLL/obj/Debug/net8.0/GatewayService.BLL.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayService.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0036e55b4f62b88e11022f8ba77ec9fd6b46bd53")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5d1e2bd2ad59a64424ed6cf6182aa46159cff4")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f23584186eedfaee29183e407123656a049dd57")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayService.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayService.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
